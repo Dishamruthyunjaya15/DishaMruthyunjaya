705627cb700d409ae09b0a5b759ed5f4df8c0bea
Dishamruthyunjaya15 <dishamruthyunjaya15@gmail.com>
2024-09-26 14:48:56 +0530

    Add files via upload

---
+++ space counter.py
@@ -0,0 +1,14 @@
+
+"""
+Created on Thu Sep 26 12:33:49 2024
+
+@author: SPURGE
+"""
+
+def count_spaces(S):
+    return S.count(' ')
+
+# Example usage
+S =input()
+space_count = count_spaces(S)
+print(space_count)
