1f365845193a02519bba1b712b99181b47122a62
Dishamruthyunjaya15 <dishamruthyunjaya15@gmail.com>
2024-09-26 14:39:16 +0530

    Add files via upload

---
+++ dog age.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 12:29:40 2024
+
+@author: SPURGE
+"""
+
+def dog_age_in_human_years(N):
+    return N * 7
+N=int(input())
+print(dog_age_in_human_years(N))
